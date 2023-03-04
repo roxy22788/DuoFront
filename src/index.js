5b5dd573f5309cccf1b02a81061e867fc17e8768
Fábio Oliveira <roxy2298gm@gmail.com>
2023-03-04 19:36:57 -0300

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Cadastro from './pages/cadastro/Cadastro';
 import Inicial from './pages/inicial/Inicial';
 import Login from './pages/login/Login';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Inicial />
+  },
+  {
+    path: "/cadastro",
+    element: <Cadastro />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
